71f6dcb4628c081b19ded6d19216280d6b0b6bfe
Eduardo <lourenco.e@pucpr.edu.br>
2024-08-29 20:41:46 -0300

    [add] form

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function adicionar() {
-  const name = document.getElementById("name");
-  const email = document.getElementById("email");
+const users = [];
 
-  const tableTbody = document.querySelector("#table tbody");
+function adicionar() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  if(name && email) {
+    users.push({ name, email });
+    renderizarTabela();
+  }
+}
+
+function renderizarTabela() { 
+  const tableTbody = document.querySelector("#users-table tbody");
+  tableTbody.innerHTML = "";
+
+  users.forEach((user, index) => {
+    const newRow = document.createElement("tr");
+
+    const idColumn = document.createElement("td");
+    idColumn.textContent = index + 1;
+
+    const nameColumn = document.createElement("td");
+    nameColumn.textContent = user.name;
+
+    const emailColumn = document.createElement("td");
+    emailColumn.textContent = user.email;
+
+    newRow.appendChild(idColumn);
+    newRow.appendChild(nameColumn);
+    newRow.appendChild(emailColumn);
+
+    tableTbody.appendChild(newRow);
+  })
 }
