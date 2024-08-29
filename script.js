cc2cb823bef36cbec570321dee293bffe345a1e2
Eduardo <lourenco.e@pucpr.edu.br>
2024-08-29 20:31:47 -0300

    [add] form

---
+++ script.js
@@ -0,0 +1,6 @@
+function adicionar() {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+
+  const tableTbody = document.querySelector("#table tbody");
+}
